736fb00b175b854b5d0b52086ad84f6733ef715c
Nikita <ovramenko.nikitka@gmail.com>
2024-07-15 07:32:17 -0400

    added a lot

--- script.js
+++ script.js
@@ -7,27 +7,27 @@ let humanChoice = 0;
 let botChoice = 0;
 
 
-alert("This is RPS game");
+// alert("This is RPS game");
 let bool = true;
 
-while(bool == true){
-botChoice = randomIntFromInterval(1,3);
-humanChoice = prompt("This is RPS\n1.Rock\n2.Paper\n3.Scissor\n"+ "Human Score: " + humanScore + "\nBot Score: " + botScore +"\nBot Choice: " + botChoice);
+// while(bool == true){
+// botChoice = randomIntFromInterval(1,3);
+// // humanChoice = prompt("This is RPS\n1.Rock\n2.Paper\n3.Scissor\n"+ "Human Score: " + humanScore + "\nBot Score: " + botScore +"\nBot Choice: " + botChoice);
 
-if(botChoice == humanChoice){
-    alert("draw!");
-}
+// if(botChoice == humanChoice){
+//     // alert("draw!");
+// }
 
-if((humanChoice == 1 && botChoice == 3) || (humanChoice == 2 && botChoice == 1) || (humanChoice == 3 && botChoice == 2)){
-    humanWon();
-}
+// if((humanChoice == 1 && botChoice == 3) || (humanChoice == 2 && botChoice == 1) || (humanChoice == 3 && botChoice == 2)){
+//     // humanWon();
+// }
 
-else if((botChoice == 1 && humanChoice == 3) || (botChoice == 2 && humanChoice == 1) || (botChoice == 3 && humanChoice == 2)){
-    botWon()
-}
+// else if((botChoice == 1 && humanChoice == 3) || (botChoice == 2 && humanChoice == 1) || (botChoice == 3 && humanChoice == 2)){
+//     // botWon()
+// }
     
 
-}
+// }
 
 
 
